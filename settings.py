2705f76c214a141c66e2cc52dad4c7a323de5313
khalil <khalilbenmeiane@gmail.com>
2024-06-03 02:43:01 +0100

    added the setting file

---
+++ settings.py
@@ -0,0 +1,16 @@
+# import os
+# todo uncomment those variable
+# HOST = os.environ.get("HOST")
+# DB_PORT = os.environ.get("DB_PORT")
+# DB_NAME = os.environ.get("DB_NAME")
+# USER = os.environ.get("USER")
+# PASSWD = os.environ.get("PASSWD")
+# JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY")
+
+DEV_MODE = True
+
+jwt_secret_key = "a2hhbGlsQ29kZTR5b3UvOi8K"
+jwt_exp_date_token = 7
+DB_NAME = "khalil"
+DB_USER = "khalil"
+DB_PASS = "khalil"
